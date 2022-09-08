ea38a2bdd1b10b602aaadf7a9d3df4d9b17e42ba
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-09-08 13:53:42 -0500

    clean working tree

--- src/component/SimpleInput.js
+++ src/component/SimpleInput.js
@@ -1,15 +1,41 @@
-import styles from "./SimpleInput.module.css"
+import React, { useState, useRef } from 'react'
+
+import styles from './SimpleInput.module.css'
 const SimpleInput = () => {
+  const nameRef = useRef('')
+  const [name, setName] = useState('')
+  const [isNameValid, setIsNameValid] = useState(true)
+//   const [wasNameVisited, setWasNameVisited ] = useState(false)
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault()
+    if (nameRef.current.value === '') {
+      setIsNameValid(false)
+      return
+    }
+    setIsNameValid(true)
+    setName(nameRef.current.value)
+
+    console.log({
+      name: nameRef.current.value,
+    })
+    setName("")
+  }
+
+  const nameChangeHandler = (event) => {
+    setName(event.target.value)
+  }
   return (
     <div className={styles.formContainer}>
       <form className={styles.form}>
         <div className={styles.formGroup}>
           <label htmlFor="name"></label>
-          <input type="text" id="name" />
+          <input type="text" id="name" ref={nameRef} value={name} onChange={nameChangeHandler} />
         </div>
         <div className={styles.formGroup}>
-            <button>Submit</button>
+          <button onClick={formSubmitHandler}>Submit</button>
         </div>
+        <div>{isNameValid ? '' : 'name can NOT be empty'}</div>
       </form>
     </div>
   )
