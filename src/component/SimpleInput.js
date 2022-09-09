a471ed3922b98596e1d460a230c14b6d6e4e96fa
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-09-09 10:16:16 -0500

    redo input and form validation

--- src/component/SimpleInput.js
+++ src/component/SimpleInput.js
@@ -1,17 +1,73 @@
-
+import React, { useState, useEffect } from 'react'
 
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState('')
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false)
+  const [nameTouched, setNameTouched] = useState(false)
+  const [formIsInvalid, setFormIsInvalid] = useState(true)
+
+  useEffect(() => {
+    setFormIsInvalid(!enteredNameIsValid)
+  }, [enteredNameIsValid])
+
+  const nameChangeHandler = (event) => {
+    setNameTouched(true)
+    setEnteredName(event.target.value)
+    if (event.target.value === '') {
+      setEnteredNameIsValid(false)
+    } else {
+      setEnteredNameIsValid(true)
+    }
+  }
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault()
+    console.log('form submiting...')
+    setNameTouched(true)
+
+    if (enteredName.trim() === '') {
+      setEnteredNameIsValid(false)
+      return
+    }
+    setEnteredNameIsValid(true)
+
+    console.log({
+      name: enteredName,
+    })
+
+    setEnteredName('')
+    setNameTouched(false)
+    setEnteredNameIsValid(false)
+  }
+
+  const nameLostFocus = (event) => {
+    setNameTouched(true)
+    console.log(event)
+    if (enteredName === '') {
+      setEnteredNameIsValid(false)
+    }
+  }
+
+  const nameIsNotValid = !enteredNameIsValid && nameTouched
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmitHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          onChange={nameChangeHandler}
+          onBlur={nameLostFocus}
+          value={enteredName}
+        />
+        {nameIsNotValid && <p>name can NOT be empty</p>}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={formIsInvalid}>Submit</button>
       </div>
     </form>
-  );
-};
+  )
+}
 
-export default SimpleInput;
+export default SimpleInput
